f8fed92bde8c5fa719ba8bb9e3d8c4842cb04285
lolu1032 <soowkw@naver.com>
2025-11-18 20:10:03 +0900

    가비지컬렉터 파악 연습 1일차

---
+++ src/main/java/com/example/cs/twoWeek/GCTest.java
@@ -0,0 +1,16 @@
+package com.example.cs.twoWeek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GCTest {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            list.add(new Object()); // 힙에 확실히 남음
+        }
+
+        System.out.println("Done");
+    }
+}
